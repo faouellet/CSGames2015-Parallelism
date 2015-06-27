f35f37cee00f2457ce735bb871725a1eae907de0
faouellet <faouellet@gmail.com>
2015-06-27 16:00:49 -0400

    Forgot the delete the old bash file

--- start_server.sh
+++
@@ -1,2 +0,0 @@
-DATA_FOLDER=./data
-src/Server "${DATA_FOLDER}/maze_small.bin" "${DATA_FOLDER}/sudoku_small.bin" "${DATA_FOLDER}/array_small.bin" "${DATA_FOLDER}/password_small.bin" "${DATA_FOLDER}/tree_small.bin" "${DATA_FOLDER}/RLE_small.bin"
