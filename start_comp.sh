022679955e9ac79ed233e9276b7a0c0514c03bc2
faouellet <faouellet@gmail.com>
2015-06-27 15:59:09 -0400

    The bash file will now start the whole competition in two separate terminals

---
+++ start_comp.sh
@@ -0,0 +1,4 @@
+DATA_FOLDER=./data
+gnome-terminal -x src/Server "${DATA_FOLDER}/maze_small.bin" "${DATA_FOLDER}/sudoku_small.bin" "${DATA_FOLDER}/array_small.bin" "${DATA_FOLDER}/password_small.bin" "${DATA_FOLDER}/tree_small.bin" "${DATA_FOLDER}/RLE_small.bin"
+
+gnome-terminal
